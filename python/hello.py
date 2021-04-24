dd0886dd8590e476b8b5a6f1c0d235b84d1d169f
NTLi <NTLi@fungomni.com>
2021-04-24 13:23:07 +0800

    python

--- python/hello.py
+++ python/hello.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
-print('hello world');
+print('hello world')
+if 5>2:
+    print('five is greater than 2')
+x = 5
+y = 'John'
+print(x)
+print(y)
+x = "awesome"
+print("ptyon is "+x)
 
