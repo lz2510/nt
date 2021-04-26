3fe76a6d07953f83d9edd11aecc66ba9a617f4fc
NTLi <NTLi@fungomni.com>
2021-04-26 22:40:18 +0800

    hello java

---
+++ java/Hello.java
@@ -0,0 +1,18 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("hello world!!");
+        String name = "John";
+        System.out.println(name);
+        int myNum = 15;
+        System.out.println(myNum);
+        int myNum2;
+        myNum2 = 20;
+        System.out.println(myNum2);
+    }
+}
+
+/*public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}*/
